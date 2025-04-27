295f02fd2f27b38065ce844f3f49bdbdcec30a6a
midnightqcz <Chengzoq@uci.edu>
2025-04-27 13:31:03 -0700

    Update player's idle.
    Fixed the error by deleting unused library: using Unity.Android.Gradle.Manifest;

--- Assets/Scripts/Battle/Behavior/GrandWitchBehavior.cs
+++ Assets/Scripts/Battle/Behavior/GrandWitchBehavior.cs
@@ -5,7 +5,6 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Unity.Android.Gradle.Manifest;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
