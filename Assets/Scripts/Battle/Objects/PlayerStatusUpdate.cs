7b4711b969b43e2ce014cc0e25610c8a1f90648d
Photon cat <lkstc112233@users.noreply.github.com>
2025-04-27 22:48:33 -0700

    Add animation for player.

--- Assets/Scripts/Battle/Objects/PlayerStatusUpdate.cs
+++ Assets/Scripts/Battle/Objects/PlayerStatusUpdate.cs
@@ -3,6 +3,8 @@ using static UnityEngine.EventSystems.EventTrigger;
 
 public class PlayerStatusUpdate : MonoBehaviour
 {
+    [SerializeField]
+    private Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,7 +15,12 @@ public class PlayerStatusUpdate : MonoBehaviour
     void Update()
     {
         var player = GetComponentInParent<LevelManager>().player;
-        transform.position = player.position;
+        Vector3 newPos = player.position;
+        animator.SetBool("is_moving", !transform.localPosition.Equals(newPos));
+        animator.SetBool("is_in_air", newPos.y > 0.05);
+        animator.SetBool("is_failing", newPos.y - transform.localPosition.y < 0);
+        transform.position = newPos;
+
         GetComponent<SpriteRenderer>().flipX = !player.facingEast;
     }
 }
