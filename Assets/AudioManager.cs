ea24e58212756ce0129af45fdac31b95e08de767
midnightqcz <Chengzoq@uci.edu>
2025-05-03 20:08:20 -0700

    little ui and soundmanager update. going to update some more soon.

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Audio Sources")]
+    public AudioSource musicSource;
+    public AudioSource SFXSource;
+
+    [Header("Audio Clips")]
+    public AudioClip Spring;
+    public AudioClip SpringR;
+    public AudioClip Summer;
+    public AudioClip SummerR;
+    public AudioClip Fall;
+    public AudioClip FallR;
+    public AudioClip Winter;
+    public AudioClip WinterR;
+    public AudioClip BossFight;
+    public AudioClip Monster_moving_SFX;
+    public AudioClip SomeotherSFX;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static AudioManager instance;
+    private void Awake()
+    {
+        if(instance ==null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void Start()
+    {
+        PlayMusic(Spring);
+    }
+    public void PlayMusic(AudioClip musicClip)
+    {
+        if(musicSource != null && musicClip != null)
+        {
+            musicSource.clip = musicClip;
+            musicSource.loop = true;
+            musicSource.Play();
+        }
+    }
+    public void PlaySFX(AudioClip sfxClip)
+    {
+        if(SFXSource != null && sfxClip != null)
+        {
+            SFXSource.PlayOneShot(sfxClip);
+        }
+    }
+    public void StopMusic()
+    {
+        if(musicSource != null)
+        {
+            musicSource.Stop();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
